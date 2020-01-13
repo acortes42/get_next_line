20d134a985fceac08871f2676594bd9c344172be
Adrian Cortes espejo <acortes-@c2r19s2.42madrid.com>
2020-01-13 12:47:38 +0100

    Berenjena

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acortes- <acortes-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/14 16:12:18 by acortes-          #+#    #+#             */
+/*   Updated: 2019/12/14 16:20:53 by acortes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <strings.h>
+# include <ctype.h>
+# include <fcntl.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(char *str, int c);
+char	*ft_strcdup(const char *s1, int c);
+int		get_next_line(int fd, char **line);
+#endif
